577960d45810124f04d86ada3464ef20b45347bf
Hyun Ho "John" An <john_an@nate.com>
2023-07-03 18:23:36 +0900

    Fix: modified config.js
    * updated to use dotenv module & .env files
    * updated .gitignore & .npmignore

--- config/config.js
+++ config/config.js
@@ -3,17 +3,29 @@
 const dotenv = require("dotenv");
 const fs = require("fs");
 const path = require("path");
+const debugError = require("debug")("error");
 
-// Set 'prod' as default NODE_ENV
-process.env.NODE_ENV = process.env.NODE_ENV || "prod";
-const isProd = process.env.NODE_ENV === "prod";
-const envFile = isProd ? ".env_prod" : ".env_dev";
+const envFile = (() => {
+  // Set 'prod' as default NODE_ENV
+  process.env.NODE_ENV = process.env.NODE_ENV || "prod";
+  switch (process.env.NODE_ENV) {
+    case "dev":
+    case "develop":
+    case "development":
+      return ".env.dev";
+    case "local":
+      return ".env.local";
+    default:
+      return ".env.prod";
+  }
+})();
 const envPath = path.join(__dirname, envFile);
 
 if (fs.existsSync(envPath)) {
   let _env = dotenv.config({ path: envPath });
   if (_env.error) {
     console.error("Not found environment file ...");
+    debugError("Full Error : ", _env.error);
   }
 } else {
   console.error("Not found environment file ...");
